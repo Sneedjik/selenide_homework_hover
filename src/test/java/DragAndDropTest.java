1e9b99c3c7ca0efef1d2f9b59d49a8f9a1cfad87
Sergei Volkov <Sneedjik@gmail.com>
2024-09-21 00:46:04 +0600

    added DragAndDropTest

---
+++ src/test/java/DragAndDropTest.java
@@ -0,0 +1,27 @@
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.interactions.Actions;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Configuration.*;
+import static com.codeborne.selenide.Selenide.*;
+
+public class DragAndDropTest {
+
+    @BeforeAll
+    static void setup() {
+        browserSize = "1920x1080";
+        baseUrl = "https://the-internet.herokuapp.com/";
+    }
+
+    @Test
+    void dragAndDropTest() {
+        open("/drag_and_drop");
+        Actions actions = new Actions(Selenide.webdriver().object());
+        actions.dragAndDrop($("#column-a"), $("#column-b")).perform();
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+    }
+
+}
