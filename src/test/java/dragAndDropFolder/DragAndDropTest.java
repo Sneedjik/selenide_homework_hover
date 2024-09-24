d9e87217766836ba4c15b663fd9f1b27f8def48d
Sergei Volkov <Sneedjik@gmail.com>
2024-09-24 17:23:06 +0600

    change DragAndDrop method

--- src/test/java/dragAndDropFolder/DragAndDropTest.java
+++ src/test/java/dragAndDropFolder/DragAndDropTest.java
@@ -1,13 +1,12 @@
 package dragAndDropFolder;
 
-import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.interactions.Actions;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Configuration.baseUrl;
 import static com.codeborne.selenide.Configuration.browserSize;
+import static com.codeborne.selenide.DragAndDropOptions.to;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -16,7 +15,7 @@ public class DragAndDropTest {
     @BeforeAll
     static void setup() {
         browserSize = "1920x1080";
-        baseUrl = "https://the-internet.herokuapp.com/";
+        baseUrl = "https://the-internet.herokuapp.com";
     }
 
     @Test
@@ -24,8 +23,9 @@ public class DragAndDropTest {
         open("/drag_and_drop");
         $("#column-a").shouldHave(text("A"));
         $("#column-b").shouldHave(text("B"));
-        Actions actions = new Actions(Selenide.webdriver().object());
-        actions.dragAndDrop($("#column-a"), $("#column-b")).perform();
+//        Actions actions = new Actions(Selenide.webdriver().object());
+//        actions.dragAndDrop($("#column-a"), $("#column-b")).perform();
+        $("#column-a").dragAndDrop(to($("#column-b")));
         $("#column-a").shouldHave(text("B"));
         $("#column-b").shouldHave(text("A"));
     }
